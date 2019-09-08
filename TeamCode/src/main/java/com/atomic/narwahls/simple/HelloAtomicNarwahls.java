bd525f54076f2a014f84b22a13f94613378d3d95
santharaam <santharaam@gmail.com>
2019-09-08 12:53:13 -0500

    Print hello - commit

--- TeamCode/src/main/java/com/atomic/narwahls/simple/HelloAtomicNarwahls.java
+++ TeamCode/src/main/java/com/atomic/narwahls/simple/HelloAtomicNarwahls.java
@@ -1,4 +1,10 @@
 package com.atomic.narwahls.simple;
 
 public class HelloAtomicNarwahls {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to FTC programming!");
+
+    }
 }
