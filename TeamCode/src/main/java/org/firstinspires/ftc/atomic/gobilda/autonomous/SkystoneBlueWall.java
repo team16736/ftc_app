7d5855d08a64ea12c2b4acf4a767fcf3c623119e
rahulsantha2006 <rahul.santha.2006@gmail.com>
2019-12-29 16:54:00 -0600

    Blue skystone collection has been completed :D

--- TeamCode/src/main/java/org/firstinspires/ftc/atomic/gobilda/autonomous/SkystoneBlueWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/atomic/gobilda/autonomous/SkystoneBlueWall.java
@@ -27,43 +27,71 @@ public class SkystoneBlueWall extends PullFoundation {
         sleep(1000);
 
         // Step --> use sensor and find stone
-        foundStone = true;
 
-        // Deliver, if stone found
+        // ##########################
+
+            // Add code here to detect skystone using sensor
+            foundStone = true;
+
+        //##########################
+
+        // If stone is found, the collect it and deliver it
         if(foundStone){
 
+            collectStoneAndDeliver(wheelActions);
         }
+
+        foundStone = false;
+
+//        // If stone is found again, the collect it and deliver it
+//        if(foundStone){
+//
+//            collectStoneAndDeliver(wheelActions);
+//        }
+
+
+        //Step8: Move backwards to park under bridge
+        drive_ReverseAndStop(wheelActions, SPEED, 0.5);
+        sleep(1000);
+
+    }
+
+    /**
+     * This method will collect stone and deliver.
+     * This method can be used again for 2nd stone.. collect and deliver
+     */
+    private void collectStoneAndDeliver(MecanumDriveWheelActions wheelActions) {
 
         //Step 2: if detect black block; Strafe RIGHT
         strafe_RightAndStop(wheelActions,SPEED,0.5);
         sleep(1000);
 
+
         //Step 3: Move FORWARD
         drive_ForwardAndStop(wheelActions, SPEED, 0.6);
         sleep(2000);
+
 
         //Step 4: Spin LEFT
         set_Direction_SpinLeft(wheelActions);
         wheelActions.driveByTime(this, 0.3, 2.2);
         sleep(2000);
 
+
         //Step 5: Move FORWARD
         drive_ForwardAndStop(wheelActions, SPEED, 0.8);
         sleep(1000);
+
 
         //Step6: Spin RIGHT to face the BLUE bridge
         set_Direction_SpinRight(wheelActions);
         wheelActions.driveByTime(this, 0.3, 0.3);
         sleep(2000);
 
+
         // Step 7: Move FORWARD and deliver to the other side of the bridge
         drive_ForwardAndStop(wheelActions, SPEED, 1.7);
         sleep(1000);
-
-        //Step8: Move backwards to park under bridge
-        drive_ReverseAndStop(wheelActions, SPEED, 0.5);
-        sleep(1000);
-
-
     }
+
 }
